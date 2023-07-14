0b3601022b3ba5bb2fabb21e3675776c774d8a17
Nicolas WYSEUR <nwyseur@paul-f3Br5s1.clusters.42paris.fr>
2023-07-14 12:58:20 +0200

    focus rendering d image

--- srcs/draw_suite.c
+++ srcs/draw_suite.c
@@ -6,8 +6,24 @@
 /*   By: nwyseur <nwyseur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 12:16:23 by nwyseur           #+#    #+#             */
-/*   Updated: 2023/07/14 12:18:02 by nwyseur          ###   ########.fr       */
+/*   Updated: 2023/07/14 12:47:34 by nwyseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cube_includes.h"
+
+void	ft_data_init(t_game *game, double *data)
+{
+	game->texture.x *= game->wall.texture_to_render->width;
+	data[0] = 1.0 * game->wall.texture_to_render->height
+		/ (game->res.y / game->perpwalldist); // ici
+	data[1] = (game->draw.drawstart - game->res.y / 2
+			+ (game->res.y / game->perpwalldist) / 2) * data[0];
+}
+
+void	ft_render_line(t_game *game, int x)
+{
+	double	data[3];
+
+
+}
